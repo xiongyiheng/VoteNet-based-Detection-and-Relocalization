71485c0be01a951e7d8e81d9fda2c0e94ca9100f
yiheng.xiong <37469008+xiongyiheng@users.noreply.github.com>
2022-05-02 11:47:59 +0200

    Update box_util.py

--- utils/box_util.py
+++ utils/box_util.py
@@ -13,6 +13,11 @@ from __future__ import print_function
 import numpy as np
 from scipy.spatial import ConvexHull
 
+def extract_pc_in_box3d(pc, box3d):
+    ''' pc: (N,3), box3d: (8,3) '''
+    box3d_roi_inds = in_hull(pc[:,0:3], box3d)
+    return pc[box3d_roi_inds,:], box3d_roi_inds
+
 def polygon_clip(subjectPolygon, clipPolygon):
    """ Clip a polygon with another polygon.
 
